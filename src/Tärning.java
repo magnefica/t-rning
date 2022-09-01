e41cf4196be3e910ca43aae06c84650321477c89
magnefica <sebastian.nordlund@futuregames.nu>
2022-09-01 17:19:48 +0200

    hejhej1

---
+++ src/Tärning.java
@@ -0,0 +1,16 @@
+
+public class Tärning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int value;
+
+        value = (int)(Math.random() * 6)+1;
+        
+        System.out.println(value);
+        System.out.println("hej");
+
+	}
+
+}
